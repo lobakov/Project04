2f6273adef6f23c5c023e6e73cbf005325e0f5fd
Sasha <sashas.p@ya.ru>
2021-09-06 13:32:44 +0300

    DiscussionStorage fix

--- src/main/java/com/db/edu/server/storage/DiscussionStorage.java
+++ src/main/java/com/db/edu/server/storage/DiscussionStorage.java
@@ -1,11 +1,12 @@
 package com.db.edu.server.storage;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class DiscussionStorage {
-    static Map<Integer, Set<Integer>> discussions;
+    static private Map<Integer, Set<Integer>> discussions = new HashMap<>();
 
     public static Set<Integer> getUsersById(int discussionId) {
         if (discussions.get(discussionId) == null) {
