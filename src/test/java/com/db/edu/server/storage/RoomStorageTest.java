17f1e2d9955e52d0d5393b1b34a659aaddbbeab5
Sasha <sashas.p@ya.ru>
2021-09-07 15:52:05 +0300

    test fix

--- src/test/java/com/db/edu/server/storage/RoomStorageTest.java
+++ src/test/java/com/db/edu/server/storage/RoomStorageTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.HashSet;
 
 import static com.db.edu.server.storage.RoomStorage.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,7 +32,7 @@ class RoomStorageTest {
 
         loadAllRooms(getRoomFolder("src/test/resources/room/"));
 
-        assertEquals(getNamesToIds().keySet(), Set.of("room1", "room2"));
+        assertEquals(getNamesToIds().keySet(), new HashSet<>(Arrays.asList("room1", "room2")));
         assertEquals(getIdsToMembers().size(), 2);
 
         Files.delete(Paths.get(roomPath + "/room1.txt"));
@@ -44,7 +45,7 @@ class RoomStorageTest {
 
         loadAllRooms(getRoomFolder("src/test/resources/room/"));
 
-        assertEquals(getNamesToIds().keySet(), Set.of());
+        assertEquals(getNamesToIds().keySet(), new HashSet<>());
         assertEquals(getIdsToMembers().size(), 0);
     }
 }
